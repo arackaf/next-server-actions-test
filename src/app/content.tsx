22795c2c50f5801953d39d6d0ab5d86fab69b442
Adam Rackis <adamrackis@hotmail.com>
2024-03-06 09:37:30 -0600

    repro

---
+++ src/app/content.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { FC, useTransition } from "react";
+import { serverActionA, serverActionB } from "./actions";
+
+export const Content: FC<{}> = () => {
+  const [running, startTransition] = useTransition();
+  const [runningB, startTransitionB] = useTransition();
+
+  function runBoth() {
+    serverActionA();
+    serverActionB();
+    // startTransition(() => serverActionA());
+    // startTransitionB(() => serverActionB());
+  }
+
+  return (
+    <div className="p-4">
+      <button onClick={runBoth}>Run</button>
+    </div>
+  );
+};
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { Content } from "./content";
+
+export default function Home() {
+  return (
+    <main>
+      <Content />
+    </main>
+  );
+}
